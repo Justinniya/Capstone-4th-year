834d70ecb8672a86669fabc0a5bb44cf890ea769
justinniya <justindelavega00@gmail.com>
2025-02-08 06:48:15 +0800

    graduating list

--- main.py
+++ main.py
@@ -24,14 +24,13 @@ def login():
         if username == 'admin' and password == 'admin':
             return redirect(url_for('dashboard'))
         else:
-            return redirect(url_for('login'))
-    return render_template("login.html")
+            return redirect(url_for('landing'))
 
 
 @app.route('/dashboard')
 def dashboard():
     cursor = mysql.connection.cursor()
-    cursor.execute("SELECT * FROM students")
+    cursor.execute("SELECT * FROM students ORDER BY name")
     students = cursor.fetchall()
 
     return render_template("dashboard.html", students=students)
@@ -39,6 +38,7 @@ def dashboard():
 @app.route('/add_students',methods=['POST','GET'])
 def add_student():
     if request.method == "POST":
+        ui_id = request.form.get('ui_id')
         name = request.form.get('fullname')
         age = request.form.get('age')
         section = request.form.get('section')
@@ -46,16 +46,27 @@ def add_student():
         major = request.form.get('major')
         status = request.form.get('status')
         cursor = mysql.connection.cursor()
-        cursor.execute(f"INSERT INTO students (name, age, year, section, major, status) Values ('{name}','{age}','{section}','{year}','{major}','{status}')")
+        cursor.execute(f"INSERT INTO students (ui_id,name, age, year, section, major, status) Values ('{ui_id}','{name}','{age}','{section}','{year}','{major}','{status}')")
         mysql.connection.commit()
         return redirect('/dashboard')
     return render_template('add_students.html')
 
-@app.route('/delete/<int:id>',methods=["GET"])
-def delete(id):
+@app.route('/confirm/<string:id>', methods=["GET"])
+def confirmation(id):
     cursor = mysql.connection.cursor()
-    cursor.execute(f"DELETE FROM students WHERE id = {id}")
+    cursor.execute(f"UPDATE students SET confirmation = '1' WHERE ui_id = {id}")
     mysql.connection.commit()
     return redirect('/dashboard')
+
+
+@app.route('/delete/<string:id>',methods=["GET"])
+def delete(id):
+    cursor = mysql.connection.cursor()
+    cursor.execute(f"DELETE FROM students WHERE ui_id = {id}")
+    mysql.connection.commit()
+    return redirect('/dashboard')
+
+
+# main function
 if __name__ == '__main__':
     app.run(debug=True)
